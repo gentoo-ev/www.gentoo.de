gentoo-ev/www.gentoo.de

commit bebc59ed60d7028377a91406101d6b345b1e3abd

Dockerfile: Resolve over-eager "COPY . /root/site/"

Dockerfile
@@ -14,9 +14,18 @@ RUN gem install jekyll
 RUN jekyll -v
 
 # Render website
-COPY .  /root/site/
-RUN ln -s /var/www/default /root/site/_site
 WORKDIR /root/site
+COPY _includes/ _includes/
+COPY _layouts/ _layouts/
+COPY _posts/ _posts/
+COPY css/ css/
+COPY dokumentation/ dokumentation/
+COPY downloads/ downloads/
+COPY img/ img/
+COPY kontakt/ kontakt/
+COPY support/ support/
+COPY _config.yml index.html ./
+RUN ln -s /var/www/default _site
 RUN jekyll build
 
 # Serve website

commit a9f6159a56860c8bf79de8e5b617bdaf14ccdc96

Dockerfile: Copy was missing a directory
Fixes: bebc59ed60d7028377a91406101d6b345b1e3abd

Dockerfile
@@ -15,6 +15,7 @@ COPY _includes/ _includes/
 COPY _layouts/ _layouts/
 COPY _posts/ _posts/
 COPY css/ css/
+COPY datenschutz/ datenschutz/
 COPY dokumentation/ dokumentation/
 COPY downloads/ downloads/
 COPY img/ img/